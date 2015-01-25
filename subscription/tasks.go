d85e19daba355c2c548867435db065b811a1d449
Declan Traynor <coderdex@gmail.com>
2015-01-25 11:23:40 +0000

    modify Subscribe task handler to accept string values for lat/lng

--- subscription/tasks.go
+++ subscription/tasks.go
@@ -15,14 +15,14 @@ const instagramClientId string = "f0a3b8daa2944138816c1ed7cd91f666"
 const instagramClientSecret string = "1e7869e58ae6463fbb6468eb6b9a7490"
 const ngrokProxyURL string = "http://6173d145.ngrok.com"
 
-var Subscribe = delay.Func("subscribe", func(c appengine.Context, channelId int, lat float64, lng float64) {
+var Subscribe = delay.Func("subscribe", func(c appengine.Context, channelId int, lat string, lng string) {
 	params := url.Values{
 		"client_id":     {instagramClientId},
 		"client_secret": {instagramClientSecret},
 		"object":        {"geography"},
 		"aspect":        {"media"},
-		"lat":           {fmt.Sprintf("%f", lat)},
-		"lng":           {fmt.Sprintf("%f", lng)},
+		"lat":           {lat},
+		"lng":           {lng},
 		"radius":        {"5000"},
 		"callback_url":  {fmt.Sprintf("%s/webhook/", ngrokProxyURL)},
 	}
