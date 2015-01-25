a6f7ccc370130699e4410e79247a0664023dd251
Robert Falconer <inbox@robertfalconer.com>
2015-01-25 10:50:21 +0000

    Change parseTempalte to renderTemplate. For reals this time.

--- web/web.go
+++ web/web.go
@@ -16,7 +16,7 @@ func init() {
 func rootHandler(writer http.ResponseWriter, request *http.Request) {
 	params := map[string]string{"": ""}
 
-	parseTemplate("search", params, writer, request)
+	renderTemplate("search", params, writer, request)
 }
 
 func resultsHandler(writer http.ResponseWriter, request *http.Request) {
@@ -31,7 +31,7 @@ func resultsHandler(writer http.ResponseWriter, request *http.Request) {
 		return
 	}
 	params := map[string]string{"token": token, "channelId": channelIdentifer}
-	parseTemplate("results", params, writer, request)
+	renderTemplate("results", params, writer, request)
 }
 
 func testChannelHandler(writer http.ResponseWriter, request *http.Request) {
